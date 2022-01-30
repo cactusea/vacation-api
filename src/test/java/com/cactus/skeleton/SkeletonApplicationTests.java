d5a5c7abd80e0181744ef6a8404a06b53a9a75d1
cactusea <plzcactus@gmail.com>
2022-01-30 17:45:26 +0900

    vacation-api project init

---
+++ src/test/java/com/cactus/skeleton/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cactus.vacation-api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
